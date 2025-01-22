f94715bd2686b707e42877afd0b430ce67ca56e4
Muhammad Shoaib <shoaibaas99@gmail.com>
2025-01-22 14:32:54 +0500

    dynamic app routing done code optimize and detail page get data

--- comforty/src/types/product.ts
+++ comforty/src/types/product.ts
@@ -9,3 +9,21 @@ export interface Tproduct {
   slug: string;
 }
 
+export interface TsingleProduct {
+  _id: string;
+  title: string;
+  price: number;
+  salePrice?: number;
+  badge?: string;
+  badgeColor?: string;
+  imageUrl: string;
+  rating: number;
+  inventory: number;
+  reviews: number;
+  category: string;
+  description: string;
+  weight: number;
+  lenght: number;
+  width: number;
+  height: number;
+}
